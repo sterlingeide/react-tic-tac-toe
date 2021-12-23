86240311cf5a92b118a78a76dfe26affdc393df3
sterlingeide <sterlinga.eide@gmail.com>
2021-12-23 14:00:08 -0800

    completed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component } from 'react';
+
+// import components
+import Game from './Game';
+
+class App extends Component {
+
+    render() {
+        return (
+            <Game />
+        );
+    };
+}
+
+export default App;
